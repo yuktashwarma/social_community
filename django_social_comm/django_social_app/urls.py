044310566905ec0b628d5a45068910c22f014ab0
Yukta Sharma <yukta.sharma1@outlook.com>
2023-07-13 23:09:54 +0530

    rename

--- django_social_comm/django_social_app/urls.py
+++ django_social_comm/django_social_app/urls.py
@@ -3,7 +3,7 @@ from django_social_app.models import *
 from django_social_app.views import userView
 
 urlpatterns = [
-    path("user/view/", userView.GetUserAPIView.as_view(),name="user_list"),
+    path("user/view/", userView.GetUserAPIView.as_view(),name="get_user"),
     #path("user/create/", userView.CreateUserAPIView.as_view(),name="create_user"),
     #path("user/update/<int:pk>/",User.UpdateTodoAPIView.as_view(),name="update_user"),
     #path("user/deactivate/<int:pk>/",User.DeleteTodoAPIView.as_view(),name="deactivate_user")
